43e04b5bb2f760a99d91f5dc95ad8be32246ac88
Óscar <oscar.serrano91@gmail.com>
2025-03-05 13:30:26 +0100

    Controlador createUser

--- Backend/src/controllers/candidates.controllers.js
+++ Backend/src/controllers/candidates.controllers.js
@@ -41,21 +41,53 @@ export const getCandidate = async (req, res) =>{
     const { id } = req.params;
 
     try {
-        
+        //Consulta por id 
         const { rows } = await pool.query("SELECT * FROM candidates WHERE id = $1", [id]);
         if(rows.length === 0) return res.status(404).json({message: `El usuario con id: ${id} no existe`}); 
         res.json(rows[0]); 
 
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: `El patrón de la id: ${id} no cumple con el estándar de sintaxis`}); 
+        res.status(500).json({ error: `El patrón de la id: ${id} no cumple con el estándar de sintáxis`}); 
     }
 
 }
 
 
 export const createCandidate = async (req, res) =>{
-    //res.json({message: 'Esto es una prueba'});
+    
+    try {
+        const { name, email, password, cv, phone_number } = req.body;
+
+        //Validación de campos nulos
+        if (!name || !email || !password || !phone_number) {
+            return res.status(400).json({ message: 'Nombre, email, contraseña y tlf son requeridos' });
+        }
+
+        //Validación de email válido
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: 'El email no tiene un formato válido' });
+        }
+
+        //validación de contraseña de como mínimo 6 dígitos
+        if (password.length < 6) return res.status(400).json({message: 'La contraseña debe de contener como mínimo 6 dígitos'});
+
+        //Validación de número de teléfono, solo números y mínimo 9.
+        const phoneRegex = /^\d{9,}$/;
+        if (!phoneRegex.test(phone_number)) {
+            return res.status(400).json({ message: 'El número de teléfono debe contener al menos 9 dígitos' });
+        }
+
+        const {rows} = await pool.query(
+                        "INSERT INTO candidates (name, email, password, cv, phone_number) VALUES ($1, $2, $3, $4, $5) RETURNING*",
+                        [name, email, password, cv || null, phone_number]);
+        return res.json(rows[0]); 
+        
+    } catch (error) {
+        if(error?.code === 23505) return res.status(409).json({message: 'Este email ya está registrado'});
+        return res.status(500).json({message: 'Internal server error'}); 
+    }
 }
 
 export const updateCandidate = async (req, res) =>{
