9623f91fb5d4316154aeb304b5ebc0cf5e1ed4b1
Óscar <oscar.serrano91@gmail.com>
2025-03-14 14:26:38 +0100

    Se actualizan campos de la tabla companies

--- Backend/src/database/database.sql
+++ Backend/src/database/database.sql
@@ -16,6 +16,9 @@ CREATE TABLE companies (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+    industry VARCHAR(50),
+    description TEXT,
+    company_url TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
