47ce8406536f42239320f375ca25c257c19e8b50
Óscar <oscar.serrano91@gmail.com>
2025-04-05 17:28:16 +0200

    Se están migrando a MVC controladores companies_jobOffers y se realiza modificación de la BBDD para añadir application_count a la tabla job_offers

--- Backend/src/models/Company.js
+++ Backend/src/models/Company.js
@@ -21,3 +21,44 @@ export const findCompanyById = async (id) =>{
     return rows[0]; 
 }
 
+export const updateCompanyModel = async (name, email, description, company_url, id)=>{
+    //Consulta update
+    const { rows } = await pool.query(
+        "UPDATE companies SET name = $1, email = $2, description = $3, company_url = $4 WHERE id = $5 RETURNING*",
+        [name, email, description, company_url, id]
+    );
+    return rows[0]; 
+}
+
+export const deleteCompanyModel = async (id)=>{
+    //Consulta empresa por id, usamos rowCount porque con DELETE row.length siempre devuelve 0
+    const { rowCount } = await pool.query("DELETE FROM companies WHERE id = $1", [id]);
+    return rowCount; 
+}
+
+export const insertNewJobOffer = async (id, title, description, location, salary, education_level, study_field) =>{
+    //Consulta insert a job_offers
+    const { rows } = await pool.query(
+        `INSERT INTO job_offers (company_id, title, description, location, salary, education_level, study_field) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING title, description, location, salary, education_level, study_field`,
+        [id, title, description, location, salary, education_level, study_field]
+    );
+    return rows[0];
+}
+
+export const findJobOffersByCompany = async (id)=>{
+    //Consulta para obtener las ofertas creadas por la empresa
+    const { rows } = await pool.query(
+        `SELECT 
+        job_offers.id, 
+        job_offers.title,
+        job_offers.location, 
+        job_offers.created_at, 
+        companies.name AS company_name
+        FROM job_offers 
+        JOIN companies ON job_offers.company_id = companies.id 
+        WHERE job_offers.company_id = $1`,
+        [id]
+    ); 
+    return rows; 
+}
