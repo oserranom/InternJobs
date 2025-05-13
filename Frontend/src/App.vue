8b4ecc6374b758836d0e7321b8a2c2f86381c0fa
Óscar <oscar.serrano91@gmail.com>
2025-05-14 01:00:03 +0200

    logo y navbar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router'
+  import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
   <div>
-    <div>
+    <Navbar />
 
-    </div>
-    
     <div>
       <RouterView />
     </div>
---
+++ Frontend/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+    <nav>
+        <div class="md:flex justify-between px-10 py-5 items-center">
+
+            <RouterLink
+                :to="{name: 'home'}"
+            >
+                <img src="/Logo/ijlogo.svg" alt="Tu logo" class="h-20 w-auto" />
+            </RouterLink>
+                
+
+            <div class="md:flex justify-between">
+                <RouterLink class="mx-5">Acceso Empresas</RouterLink>
+                <RouterLink class="mx-5">Acceso Candidatos</RouterLink>
+                <RouterLink class="mx-5">Regístrate</RouterLink>
+            </div>
+        </div>
+    </nav>
+</template>
