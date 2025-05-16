0a15a58ae5b9564789782bd13986a48149701db0
Oscar <oscar.serrano91@gmail.com>
2025-05-16 20:27:22 +0200

    Se añade imagen header y se ajusta footer y navbar en tamaño pequeño

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -6,7 +6,7 @@ import { RouterLink } from 'vue-router';
 
 <template>
     <nav class="bg-gray-800 sticky top-0 z-10 shadow-[0_4px_6px_-2px_rgba(16,185,129,0.5)]">
-        <div class="md:flex justify-between px-15 py-5 items-center">
+        <div class="text-center md:flex justify-between px-15 py-5 items-center">
 
             <RouterLink
                 class="text-3xl font-serif font-bold text-gray-50 hover:text-emerald-400 transition duration-300 ease-in-out"
@@ -16,32 +16,35 @@ import { RouterLink } from 'vue-router';
             </RouterLink>
                 
 
-            <div class="md:flex justify-between">
-        
-               
-                <RouterLink 
-                    class="text-gray-50 text-lg hover:text-gray-800 mx-2 hover:bg-gray-50 transition duration-300 ease-in-out
-                            py-1 px-3 rounded-lg font-semibold"
-                >
-                    Acceso Empresas
-                </RouterLink>
-            
-                <RouterLink 
-                    class="text-gray-50 text-lg hover:text-gray-800 mx-2 hover:bg-gray-50 transition duration-300 ease-in-out
-                            py-1 px-3 rounded-lg font-semibold"
-                >
-                    Acceso Candidatos
-                </RouterLink>
-            
-                <RouterLink 
-                    class="text-lg font-bold text-gray-800 bg-emerald-400 mx-2 rounded-lg py-1 px-3 
+            <div class="text-center md:flex justify-between items-center">
+               <div class="my-1">
+                    <RouterLink 
+                        class="text-gray-50 text-lg hover:text-gray-800 mx-2 hover:bg-gray-50 transition duration-300 ease-in-out
+                            py-2 px-3 rounded-lg font-semibold"
+                    >
+                        Acceso Empresas
+                    </RouterLink>
+               </div>
+
+                <div class="my-1">
+                    <RouterLink 
+                        class="text-gray-50 text-lg hover:text-gray-800 mx-2 hover:bg-gray-50 transition duration-300 ease-in-out
+                            py-2 px-3 rounded-lg font-semibold"
+                    >
+                        Acceso Candidatos
+                    </RouterLink>
+                </div>
+
+                <div class="my-1">
+                    <RouterLink 
+                        class="text-lg font-bold text-gray-800 bg-emerald-400 mx-2 rounded-lg py-2 px-3 
                         hover:shadow-[0_8px_25px_rgba(16,185,129,0.5)] hover:bg-emerald-300 transition duration-300 ease-in-out"
-                >
-                    Regístrate
-                </RouterLink>
+                    >
+                        Regístrate
+                    </RouterLink>
+                </div>
                 
             </div>
-
 
         </div>
     </nav>
--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,11 +1,26 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <main class="w-2/3 mx-auto h-170">
-    <h1 class="text-4xl font-semibold text-white text-center mt-10">Portal de ofertas para prácticas laborales</h1>
-    <h2 class="text-emerald-400 text-center font-semibold text-2xl mt-10">De estudiante a profesional, construyendo tu futuro.</h2>
-    <p class="text-center font-light text-xl mt-2">Encuentra prácticas profesionales y formaliza tu experiencia.</p>
+  <div
+    class="relative w-full h-[250px] md:h-[400px] bg-[url('/img/office.webp')] bg-cover bg-center md:bg-[center_bottom_40%] 
+          bg-no-repeat flex items-center justify-center"
+  >
+    <h1 class="absolute top-5 text-4xl font-semibold text-gray-900 text-center drop-shadow-lg">
+      Portal de ofertas para prácticas laborales
+    </h1>
+
+    <div class="absolute w-2/3 text-center bg-gray-800/20">
+      
+    </div>
+
+  </div>
+
+  <main class="w-2/3 mx-auto h-170 mt-10">
+    <h2 class="text-emerald-400 text-center font-semibold text-2xl">
+      De estudiante a profesional, construyendo tu futuro.
+    </h2>
+    <p class="text-center font-light text-xl mt-2">
+      Encuentra prácticas profesionales y formaliza tu experiencia.
+    </p>
   </main>
 </template>
