6e30cb685db552085de3ea1ff7c3a2531c3ff6a8
Óscar <oscar.serrano91@gmail.com>
2025-05-16 13:40:18 +0200

    fin footer, con enlaces a contacto

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <main class="w-2/3 mx-auto h-150">
+  <main class="w-2/3 mx-auto h-170">
     <h1 class="text-4xl font-semibold text-white text-center mt-10">Portal de ofertas para prácticas laborales</h1>
     <h2 class="text-emerald-400 text-center font-semibold text-2xl mt-10">De estudiante a profesional, construyendo tu futuro.</h2>
     <p class="text-center font-light text-xl mt-2">Encuentra prácticas profesionales y formaliza tu experiencia.</p>
